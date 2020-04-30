425468e9fe42f0cb45878cb45918cc7b0a46a97a
Varptelion <maikl.1997@mail.ru>
2020-05-01 00:42:04 +0400

    compete Lab

--- MonoLinkedList/main.cpp
+++ MonoLinkedList/main.cpp
@@ -3,6 +3,21 @@
 
 int main()
 {
-    MonoLinkedList<int> list1();
+    MonoLinkedList<int> list1;
+    list1.insertFirst( 5 );
+    list1.insertFirst( 10 );
+    list1.insertFirst( 3 );
+    list1.insertFirst( 1231 );
+    list1.insertFirst( 5 );
+    list1.print();
+
+    std::cout << "\nis Empty: " << list1.isEmpty();
+    std::cout << "\nContains 4: " << list1.isContains( 4 );
+    std::cout << "\nCount elements: " << list1.size();
+    std::cout << "\nLast: " << list1.getLast();
+    list1.deleteFirst();
+    if( list1.deleteAtPos( 2 ) )
+    std::cout << std::endl << std::endl << std::endl;
+    list1.print();
     return 0;
 }
